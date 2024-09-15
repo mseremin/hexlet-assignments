56744c939f38dd85eb5f8e9ddd07e68c5dca51e1
@hexlet/cli <support@hexlet.io>
2024-09-15 13:59:39 +0300

    submit java-spring-ru/error-handling

--- java-spring-ru/error-handling/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/error-handling/src/main/java/exercise/controller/ProductsController.java
@@ -1,7 +1,6 @@
 package exercise.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
@@ -9,6 +8,15 @@ import java.util.List;
 import exercise.model.Product;
 import exercise.repository.ProductRepository;
 import exercise.exception.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 @RequestMapping("/products")
