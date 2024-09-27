ddeda530a4400230a6c91e6fc81972ceb81b23eb
@hexlet/cli <support@hexlet.io>
2024-09-27 15:50:28 +0300

    submit java-spring-ru/dto-to-entity

---
+++ java-spring-ru/dto-to-entity/src/main/java/exercise/controller/ContactsController.java
@@ -0,0 +1,51 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import exercise.model.Contact;
+import exercise.repository.ContactRepository;
+import exercise.dto.ContactDTO;
+import exercise.dto.ContactCreateDTO;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactsController {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    // BEGIN
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContactDTO create(@RequestBody ContactCreateDTO contactCreateDTO) {
+        Contact contact = toEntity(contactCreateDTO);
+        contactRepository.save(contact);
+        return toDto(contact);
+    }
+
+    private Contact toEntity(ContactCreateDTO contactCreateDTO) {
+        var contact = new Contact();
+        contact.setPhone(contactCreateDTO.getPhone());
+        contact.setFirstName(contactCreateDTO.getFirstName());
+        contact.setLastName(contactCreateDTO.getLastName());
+        return contact;
+    }
+
+    private ContactDTO toDto(Contact contact) {
+        var contactDto = new ContactDTO();
+        contactDto.setId(contact.getId());
+        contactDto.setFirstName(contact.getFirstName());
+        contactDto.setLastName(contact.getLastName());
+        contactDto.setPhone(contact.getPhone());
+        contactDto.setCreatedAt(contact.getCreatedAt());
+        contactDto.setUpdatedAt(contact.getUpdatedAt());
+        return contactDto;
+    }
+    // END
+}
